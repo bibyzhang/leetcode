0ea0bac05fb2ca53cdfa044481e2ffcc7b30edc1
biby <bibyzhang90@gamil.com>
2021-07-07 21:28:55 +0800

    LRU

--- LRUCache.go
+++ LRUCache.go
@@ -53,11 +53,13 @@ func (l *LRUCache) InitLink(capacity int) {
 	l.hashmap = make(map[int]*LinkNode)
 
 	head := &LinkNode{}
-
 	tail := &LinkNode{}
 
 	l.LinkList.head = head
 	l.LinkList.tail = tail
+
+	l.LinkList.head.next = tail
+	l.LinkList.tail.prev = head
 
 	l.LinkList.size = 0
 	l.LinkList.capacity = capacity
@@ -76,22 +78,27 @@ func (l *LRUCache) AddLink(key int, value int) {
 	newNode.key = key
 	newNode.value = value
 	//第一个节点
-	if(l.LinkList.head.next==nil) {
-		l.LinkList.head.next = newNode
-		l.LinkList.tail.prev = newNode
-		newNode.prev = l.LinkList.head
-	} else {
-		current := l.LinkList.head
-		//寻找最后一个节点
-		for current.next != nil {
-			current = current.next
-		}
+	//if(l.LinkList.head.next==nil) {
+	//	l.LinkList.head.next = newNode
+	//	l.LinkList.tail.prev = newNode
+	//	newNode.prev = l.LinkList.head
+	//	newNode.next = l.LinkList.tail
+	//} else {
+		//current := l.LinkList.head
+		////寻找最后一个节点
+		//for current.next != nil {
+		//	current = current.next
+		//}
+		current := l.LinkList.tail.prev
 		newNode.next = current.next
+		newNode.prev = current
 		current.next = newNode
-		current.next.prev = current
-	}
+		l.LinkList.tail.prev = newNode
+	//}
 
 	l.LinkList.size++
+	//fmt.Printf("%v", l.LinkList)
+
 	l.hashmap[key] = newNode
 }
 
@@ -107,18 +114,19 @@ func (l *LRUCache) MakeNodeRecenty(key int) {
 func (l *LRUCache) RemoveLink(key int) {
 	node := l.hashmap[key]
 
-	if(l.LinkList.size==1) {
-		l.LinkList.head = &LinkNode{}
-		l.LinkList.tail = &LinkNode{}
-	} else {
+	//if(l.LinkList.size==1) {
+	//	l.LinkList.head = &LinkNode{}
+	//	l.LinkList.tail = &LinkNode{}
+	//} else {
 		//非最后一个元素,最后一个元素next为nil,将prev的next置为nil即可
-		if(node.next!=nil) {
+		//if(node.next!=nil) {
 			node.prev.next = node.next
 			node.next.prev = node.prev
-		} else {
-			node.prev.next = nil
-		}
-	}
+		//} else {
+		//	node.prev.next = nil
+		//}
+		//l.LinkList.tail.prev = node
+	//}
 
 	l.hashmap[key] = nil
 	l.LinkList.size--
@@ -127,9 +135,12 @@ func (l *LRUCache) RemoveLink(key int) {
 //删除最旧的节点
 //最旧的节点即是头部节点
 func (l *LRUCache) RemoveLastLink() {
+	//fmt.Printf("%v", l.LinkList.head.next.key)
 	node := l.LinkList.head.next
 	key := node.key
-	l.LinkList.head = l.LinkList.head.next
+	node.next.prev = node.prev
+	node.prev.next = node.next
+	//l.LinkList.head = node
 	l.hashmap[key] = nil
 	l.LinkList.size--
 }
@@ -202,30 +213,33 @@ func main() {
 	//测试3
 	//["LRUCache","put","get","put","get","get"]
 	//[[1],[2,1],[2],[3,2],[2],[3]]
-	//obj := Constructor(1)
-	//obj.Put(2,1)
-	//param_1 := obj.Get(2)
-	//obj.Put(3,2)
-	//param_2 := obj.Get(2)
-	//param_3 := obj.Get(3)
+	obj := Constructor(1)
+	obj.Put(2,1)
+	param_1 := obj.Get(2)
+	obj.Put(3,2)
+	param_2 := obj.Get(2)
+	param_3 := obj.Get(3)
 
 	//测试4
 	//["LRUCache","get","put","get","put","put","get","get"]
 	//[[2],[2],[2,6],[1],[1,5],[1,2],[1],[2]]
-	obj := Constructor(2)
-	param_1 := obj.Get(2)
-	obj.Put(2,6)
-	param_2 := obj.Get(1)
-	obj.Put(1,5)
-	obj.Put(1,2)
-	obj.Get(2)
-	param_3 := obj.Get(1)
-	param_4 := obj.Get(2)
+	//obj := Constructor(2)
+	//param_1 := obj.Get(2)
+	//obj.Put(2,6)
+	//param_2 := obj.Get(1)
+	//obj.Put(1,5)
+	//obj.Put(1,2)
+	//obj.Get(2)
+	//param_3 := obj.Get(1)
+	//param_4 := obj.Get(2)
+
+//["LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"]
+//	[[10],[10,13],[3,17],[6,11],[10,5],[9,10],[13],[2,19],[2],[3],[5,25],[8],[9,22],[5,5],[1,30],[11],[9,12],[7],[5],[8],[9],[4,30],[9,3],[9],[10],[10],[6,14],[3,1],[3],[10,11],[8],[2,14],[1],[5],[4],[11,4],[12,24],[5,18],[13],[7,23],[8],[12],[3,27],[2,12],[5],[2,9],[13,4],[8,18],[1,7],[6],[9,29],[8,21],[5],[6,30],[1,12],[10],[4,15],[7,22],[11,26],[8,17],[9,29],[5],[3,4],[11,30],[12],[4,29],[3],[9],[6],[3,4],[1],[10],[3,29],[10,28],[1,20],[11,13],[3],[3,12],[3,8],[10,9],[3,26],[8],[7],[5],[13,17],[2,27],[11,15],[12],[9,19],[2,15],[3,16],[1],[12,17],[9,1],[6,19],[4],[5],[5],[8,1],[11,7],[5,2],[9,28],[1],[2,2],[7,4],[4,22],[7,24],[9,26],[13,28],[11,26]]
 
 	//
 	fmt.Printf("%v", param_1)
 	fmt.Printf("%v", param_2)
 	fmt.Printf("%v", param_3)
-	fmt.Printf("%v", param_4)
+	//fmt.Printf("%v", param_4)
 	//fmt.Printf("%v", param_5)
 }
