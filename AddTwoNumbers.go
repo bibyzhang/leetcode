1ee968883f7ee38d8d321fc2a6c3d4a559264a14
biby <bibyzhang90@gamil.com>
2021-07-08 19:03:01 +0800

    big_num_not_support

--- AddTwoNumbers.go
+++ AddTwoNumbers.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 )
 
-type List struct {
-	head *ListNode//指向第一个节点
-	size int
-}
+//type List struct {
+//	head *ListNode//指向第一个节点
+//	size int
+//}
 
 type ListNode struct {
 	Val int
@@ -18,20 +18,20 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	//fmt.Println("%v", l1)
-
 	var r1 int
 	key := 0
 	current := l1
 	for current.Next!=nil {
 		tmp := math.Pow10(key)
-		r1 = r1 + current.Val * int(tmp)
-		//fmt.Println("%d", current.Val)
+		fmt.Println("%v", current.Val)
+		r1 += current.Val * int(tmp)
+		//fmt.Println("%d", r1)
 		current = current.Next
 		//fmt.Println("%v", current)
 		key++
 	}
 	//最后一个数字
+	key++
 	tmp := math.Pow10(key)
 	r1 = r1 + current.Val * int(tmp)
 	//fmt.Println("%d", r1)
@@ -49,13 +49,13 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		key2++
 	}
 	//最后一个数字
+	key2++
 	tmp2 := math.Pow10(key2)
 	r2 = r2 + current2.Val * int(tmp2)
 	//fmt.Println("%d", r2)
 
 	result := r1+r2
 	//fmt.Println("%d", result)
-
 
 	//key3 := 100-1//最多100个数字,即最高位是10的99次方
 	//for i:=key3; i>=0; i-- {
@@ -68,93 +68,98 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	result_map := strings.Split(result_s, "")
 	//fmt.Println("%v", result_map)
 
-	ResultList := &List{}
-	ResultList.initList()
+	ResultListNode := &ListNode{}
 	//reverse_result_map := make(map[int]int)
 	mapleng := len(result_map)
 	var kv string
 	for i:=mapleng-1; i>=0; i-- {
 		kv = result_map[i]
 		kvi,_ := strconv.Atoi(kv)
-		ResultList.addLinkNode(kvi)
+		if(i==mapleng-1) {
+			initList(ResultListNode, kvi)
+		} else {
+			addLinkNode(ResultListNode, kvi)
+		}
 		//fmt.Println("%v", kv)
 	}
-	//for key,val := range result_map {
-	//
-	//}
 
-	return ResultList.head
+	return ResultListNode
 }
 
-func (l *List) initList()  {
-	head := &ListNode{}
-	l.head = head
-	l.size = 0
+func initList(l *ListNode, val int)  {
+	l.Val = val
+	l.Next = nil
 }
 
-func (l *List) addLinkNode(val int)  {
-	current := l.head
-	//找到最后一个节点
-	for current.Next != nil {
-		current = current.Next
-	}
+func addLinkNode(l *ListNode, val int)  {
 	newNode := &ListNode{}
 	newNode.Val = val
 	newNode.Next = nil
-	current.Next = newNode
-	l.size++
+
+	//找到最后一个节点
+	current := l
+	for current.Next != nil {
+		current = current.Next
+	}
+
+	if(l.Next == nil) {
+		l.Next = newNode
+	} else {
+		current.Next = newNode
+	}
 }
 
-//func (l *ListNode) initList(val int)  {
-//	head := &ListNode{}
-//	l.Val = val
-//	l.Next = nil
-//}
-//
-//func (l *ListNode) addLinkNode(val int)  {
-//	//找到最后一个节点
-//	var current *ListNode
-//	for l.Next != nil {
-//		current = l.Next
-//	}
-//	newNode := &ListNode{}
-//	newNode.Val = val
-//	newNode.Next = nil
-//	if(current!=nil) {//第一个元素不用
-//		current.Next = newNode
-//	}
-//}
-
 func main() {
-	var List1 List
-	List1.initList()
+	var ListNode1 ListNode
+	//initList(&ListNode1, 9)
 	//List1.addLinkNode(2)
 	//List1.addLinkNode(4)
 	//List1.addLinkNode(3)
 
 	//List1.addLinkNode(0)
 
-	List1.addLinkNode(9)
-	List1.addLinkNode(9)
-	List1.addLinkNode(9)
-	List1.addLinkNode(9)
-	List1.addLinkNode(9)
-	List1.addLinkNode(9)
-	List1.addLinkNode(9)
+	//addLinkNode(&ListNode1,9)
+	//addLinkNode(&ListNode1,9)
+	//addLinkNode(&ListNode1,9)
+	//addLinkNode(&ListNode1,9)
+	//addLinkNode(&ListNode1,9)
+	//addLinkNode(&ListNode1,9)
 
-	var List2 List
-	List2.initList()
-	//List2.addLinkNode(5)
-	//List2.addLinkNode(6)
-	//List2.addLinkNode(4)
+	initList(&ListNode1, 1)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 0)
+	addLinkNode(&ListNode1, 1)
 
-	//List2.addLinkNode(0)
 
-	List2.addLinkNode(9)
-	List2.addLinkNode(9)
-	List2.addLinkNode(9)
-	List2.addLinkNode(9)
-
+	var ListNode2 ListNode
+	initList(&ListNode2, 9)
+	////List2.addLinkNode(5)
+	////List2.addLinkNode(6)
+	////List2.addLinkNode(4)
+	//
+	////List2.addLinkNode(0)
+	//
+	addLinkNode(&ListNode2, 9)
+	addLinkNode(&ListNode2, 9)
+	addLinkNode(&ListNode2,9)
 
 	//var ListNode1 ListNode
 	//ListNode1.Val = 2
@@ -173,15 +178,15 @@ func main() {
 	//ListNode2.addLinkNode(6)
 	//ListNode2.addLinkNode(7)
 
-	//fmt.Println("%v", List1)
-	//fmt.Println("%v", List2)
+	//fmt.Println("%v", ListNode1)
+	//fmt.Println("%v", ListNode2)
 	//
 	//for ListNode ListNode := range List1 {
 	//
 	//}
 
-	result := addTwoNumbers(List1.head.Next, List2.head.Next)
-	fmt.Println("%v", result)
+	result := addTwoNumbers(&ListNode1, &ListNode1)
+	fmt.Println("%v", result.Next.Next.Next)
 
 	//addTwoNumbers()
 }
