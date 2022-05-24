6f8d28559c865b4d2ba2ba026e339ee2987ed36d
biby <bibyzhang90@gmail.com>
2022-05-24 17:02:59 +0800

    numTrees

---
+++ dp/numTrees.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i:=1; i<=n; i++ {
+		for j:=1; j<=i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+
+	return dp[n]
+}
+
+func main()  {
+	n := 3
+	num := numTrees(n)
+	fmt.Println(num)
+}
