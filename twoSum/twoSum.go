6cdade76a6b59d9d6a86a5b16e1df9378fad010c
biby <bibyzhang90@gmail.com>
2023-08-21 09:53:32 +0800

    twoSum

--- twoSum/twoSum.go
+++ twoSum/twoSum.go
@@ -5,9 +5,12 @@ import "fmt"
 func twoSum(nums []int, target int) []int {
 	hashTable := map[int]int{}
 	for key, val := range nums {
+		//每一个循环将当前值存起来
+		//判断跟当前值相加能满足条件的值是否在前面出现(hashTable存起来则出现过)，如果，拿出其key
 		if key_, ok := hashTable[target-val]; ok {
 			return []int{key_, key}
 		}
+		//没找到则没匹配到，存储起来
 		hashTable[val] = key
 	}
 
